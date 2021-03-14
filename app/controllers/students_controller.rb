ac6266fa515b8c6ad34e9afc46564dff9a2bec62
stuypend <andre@stuypend.com>
2021-03-14 14:40:54 -0400

    commit tree 0a153f2901915f264cd2b4cf33ef9f82182d1a6d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
 class StudentsController < ApplicationController
   def index
+    Student.create(first_name: "John", last_name: "Sack")
     @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
 end
